d224b13d52adc84e7eb2d99858e75f4f1c5458b7
nlauron <nlauron@telus.net>
2017-10-04 12:55:43 -0700

    Unity Maze

---
+++ Maze/Assets/Maze/Scripts/Maze.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour {
+
+    public GameObject wall;
+    public float wallLength = 1.0f;
+    public float xSize = 5.0f;
+    public float ySize = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void CreateWalls () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
