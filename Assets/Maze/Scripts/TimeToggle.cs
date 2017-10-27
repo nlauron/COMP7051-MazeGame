507a22d2e6e31f9cedc77ee6569aa93baf50d137
Niko <nlauron@telus.net>
2017-10-27 00:25:02 -0700

    Toggle Day and Night

--- Assets/Maze/Scripts/TimeToggle.cs
+++ Assets/Maze/Scripts/TimeToggle.cs
@@ -2,52 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeToggle : MonoBehaviour
-{
-    public Light sun;
-    public float secondsInFullDay = 120f;
-    [Range(0, 1)]
-    public float currentTimeOfDay = 0;
-    [HideInInspector]
-    public float timeMultiplier = 1f;
-
-    float sunInitialIntensity;
+public class TimeToggle : MonoBehaviour { 
 
     void Start()
     {
-        sunInitialIntensity = sun.intensity;
+
     }
 
     void Update()
     {
-        UpdateSun();
-
-        currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
-
-        if (currentTimeOfDay >= 1)
+        if (Input.GetKeyDown("k"))
         {
-            currentTimeOfDay = 0;
-        }
-    }
-
-    void UpdateSun()
-    {
-        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
-
-        float intensityMultiplier = 1;
-        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f)
-        {
-            intensityMultiplier = 0;
-        }
-        else if (currentTimeOfDay <= 0.25f)
-        {
-            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
-        }
-        else if (currentTimeOfDay >= 0.73f)
-        {
-            intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
+            transform.Rotate(new Vector3(180, 0, 0));
         }
 
-        sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
 }
