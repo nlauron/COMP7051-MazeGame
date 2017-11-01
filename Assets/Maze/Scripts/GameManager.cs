55ccd70acebcd91b2d54530049fb0d5d88d9e34b
Matthew Taylor <mtaylor8878@gmail.com>
2017-10-31 18:59:24 -0700

    Added new maze generation algorithm

---
+++ Assets/Maze/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+    // Use this for initialization
+    void Start() {
+        newGame();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            restart();
+        }
+    }
+
+    private void newGame() {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.generate());
+    }
+
+    private void restart() {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        newGame();
+    }
+}
