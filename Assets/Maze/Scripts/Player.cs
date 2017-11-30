8d5683eb1fb5124a905710d24443bce12a451ec5
Matthew Taylor <mtaylor8878@gmail.com>
2017-11-29 17:47:48 -0800

    added ambient lighting and toggles

--- Assets/Maze/Scripts/Player.cs
+++ Assets/Maze/Scripts/Player.cs
@@ -7,22 +7,33 @@ public class Player : MonoBehaviour {
 
     private bool toggle = true;
     public static int winCondition = 0;
+    public GameObject pov;
+    public Shader mainShader;
+
+    private bool day = true;
+    private bool fog = true;
+    private bool flashLight = true;
 
     // Use this for initialization
     void Start() {
-
+        pov.GetComponent<Camera>().SetReplacementShader(mainShader, null);
     }
 
     // Update is called once per frame
-    void Update() { 
-        if (Input.GetButton("Phase"))
+    void Update() {
+        if (Input.GetButtonDown("Phase")) {
             toggle = !toggle;
-
-        if (toggle)
-            gameObject.layer = 9;
-        else
-            gameObject.layer = 10;
-
+            gameObject.layer = toggle ? 9 : 10;
+        }
+        if (Input.GetButtonDown("Fog")) {
+            toggleFog();
+        }
+        if (Input.GetButtonDown("Day")) {
+            setTime(!day);
+        }
+        if (Input.GetButtonDown("Flashlight")) {
+            toggleFlashLight();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -38,7 +49,21 @@ public class Player : MonoBehaviour {
         {
             winCondition++;
             SceneManager.LoadScene(0);
-            Debug.Log("Hit Hit Hit");
         }
+    }
+
+    public void setTime(bool day) {
+        this.day = day;
+        Shader.SetGlobalInt("_Day", day ? 1 : 0);
+    }
+
+    public void toggleFog() {
+        fog = !fog;
+        Shader.SetGlobalInt("_Fog", fog ? 1 : 0);
+    }
+
+    public void toggleFlashLight() {
+        flashLight = !flashLight;
+        Shader.SetGlobalInt("_Light", flashLight ? 1 : 0);
     }
 }
